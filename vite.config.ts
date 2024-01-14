282eb302d63cd754cd0184879283c915aafc164e
juanuranidev <juanuranidev@gmail.com>
2024-01-13 23:41:05 -0300

    Installed react-router-dom, lodash, @ant-design/colors and some types

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src",
+      components: "/src/components",
+      layout: "/src/layout",
+      routes: "/src/routes",
+    },
+  },
+});
