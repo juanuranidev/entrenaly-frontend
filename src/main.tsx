4a81daa1036b8179b1e3c661437ac38445a5e6f3
juanuranidev <juanuranidev@gmail.com>
2024-01-21 21:29:25 -0300

    Import a font and changes in main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
+import Theme from "./theme/Theme";
 import Routes from "routes";
 import ReactDOM from "react-dom/client";
-import ThemeCustomization from "./themes";
 import { BrowserRouter as Router } from "react-router-dom";
 import "css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeCustomization>
+    <Theme>
       <Router>
         <Routes />
       </Router>
-    </ThemeCustomization>
+    </Theme>
   </React.StrictMode>
 );
