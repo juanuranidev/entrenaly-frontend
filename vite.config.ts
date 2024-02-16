934332c053b2a2573920e81b782fd4b21171c13b
juanuranidev <juanuranidev@gmail.com>
2024-02-15 23:43:55 -0500

    Updated tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": "/src",
-      components: "/src/components",
+      css: "/src/css",
+      lib: "/src/lib",
+      pages: "/src/pages",
       layout: "/src/layout",
       routes: "/src/routes",
-      pages: "/src/pages",
-      css: "/src/css",
+      components: "/src/components",
     },
   },
 });
