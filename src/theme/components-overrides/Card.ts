10e9944231cd321f58952a1735f18994e0392bc0
juanuranidev <juanuranidev@gmail.com>
2024-02-20 23:52:54 -0500

    Updated theme

--- src/theme/components-overrides/Card.ts
+++ src/theme/components-overrides/Card.ts
@@ -4,9 +4,8 @@ export default function Card(theme: any) {
       styleOverrides: {
         root: {
           padding: theme.spacing(3),
+          boxShadow: theme.customShadows.card,
           border: `1px solid ${theme.palette.divider}`,
-          boxShadow:
-            "rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px",
         },
       },
     },
--- src/theme/general/shadows.ts
+++ src/theme/general/shadows.ts
@@ -1,4 +1,5 @@
 const shadows = {
+  card: "rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px",
   primary:
     "rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px",
   secondary:
