87f862fec458dbbb8cb77972d2fa9efc55212c22
juanuranidev <juanuranidev@gmail.com>
2024-05-01 23:26:27 -0300

    Some updates

--- src/pages/trainer/plans/new-weekly-plan/NewWeeklyPlan.tsx
+++ src/pages/trainer/plans/new-weekly-plan/NewWeeklyPlan.tsx
@@ -1,20 +1,13 @@
 import { Box, Card } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import WeeklyPlanForm from "components/forms/weekly-plan-form/WeeklyPlanForm";
 import PageTitle from "components/common/page-title/PageTitle";
+import WeeklyPlanForm from "components/forms/weekly-plan-form/WeeklyPlanForm";
 
 export default function NewWeeklyPlan() {
-  const navigate = useNavigate();
-
-  const handleSubmitPlanForm = () => {
-    navigate("/plans");
-  };
-
   return (
     <Box>
       <PageTitle title="Nuevo plan semanal" />
       <Card>
-        <WeeklyPlanForm onSubmit={handleSubmitPlanForm} />
+        <WeeklyPlanForm />
       </Card>
     </Box>
   );
--- src/services/plan/plan.services.ts
+++ src/services/plan/plan.services.ts
@@ -12,3 +12,61 @@ export const getAllPlansCategoriesService = async () => {
     throw error;
   }
 };
+
+export const getAllDaysOfWeekService = async () => {
+  try {
+    const response = await request({
+      method: "GET",
+      url: "/plan/v1/get/days-of-week",
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const postWeeklyPlanService = async (data: any) => {
+  try {
+    const response = await request({
+      method: "POST",
+      url: "/plan/v1/create/weekly-plan",
+      data: {
+        data,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getAllPlansByUserIdService = async () => {
+  try {
+    const response = await request({
+      method: "GET",
+      url: "/plan/v1/get/plans-by-user-id",
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getAllPlansByClientIdService = async (clientId: string) => {
+  try {
+    const response = await request({
+      method: "GET",
+      url: "/plan/v1/get/plans-by-client-id",
+      params: {
+        clientId,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
