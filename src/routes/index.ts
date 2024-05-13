d83ab846c1aa4be093c03640d86192e60ab3537e
juanuranidev <juanuranidev@gmail.com>
2024-05-12 23:47:32 -0300

    Updated button and index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
+import { useAuthContext } from "contexts/Auth";
 import { useRoutes } from "react-router-dom";
 import PublicRoutes from "./PublicRoutes";
 import TrainerRoutes from "./TrainerRoutes";
 
 export default function Routes() {
+  const { userData } = useAuthContext();
+
   return useRoutes([TrainerRoutes, PublicRoutes]);
 }
--- src/theme/components-overrides/Button.ts
+++ src/theme/components-overrides/Button.ts
@@ -11,7 +11,9 @@ export default function Button(theme: any) {
         disableElevation: true,
       },
       styleOverrides: {
-        root: {},
+        root: {
+          borderRadius: theme?.spacing(1),
+        },
         contained: {
           fontWeight: 600,
           backgroundColor: theme?.colors?.background?.tertiary,
@@ -21,7 +23,6 @@ export default function Button(theme: any) {
           ...disabledStyle,
         },
         outlined: {
-          borderRadius: theme?.spacing(1),
           color: theme?.colors?.text?.tertiary,
           border: `1px solid ${theme?.colors?.text?.tertiary}`,
           ...disabledStyle,
