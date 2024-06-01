763785371ef4c6ab32541a5f0c739720632950c5
juanuranidev <juanuranidev@gmail.com>
2024-05-31 23:51:39 -0300

    Working on plan card

--- src/pages/client/plans/index/components/plan-card/PlanCard.tsx
+++ src/pages/client/plans/index/components/plan-card/PlanCard.tsx
@@ -10,17 +10,21 @@ type Props = {
 export default function PlanCard({ plan }: Props) {
   const { theme } = useThemeContext();
   console.log(plan);
+
   return (
     <Box
       display="flex"
-      boxShadow={theme?.customShadows.primary}
+      boxShadow={"rgba(0, 0, 0, 0.3) 0px 20px 20px -20px;"}
       p={theme?.spacing(3)}
       gap={theme?.spacing(2)}
-      bgcolor={theme?.colors?.background?.secondary}
+      sx={{ cursor: "pointer" }}
       borderRadius={theme?.spacing(1)}
+      bgcolor={theme?.colors?.background?.secondary}
     >
       <Icons.pdfs color="error" />
-      <Typography>{plan?.name}</Typography>
+      <Typography fontWeight={500} fontSize={14}>
+        {plan?.name}
+      </Typography>
     </Box>
   );
 }
