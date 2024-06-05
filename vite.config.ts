4f61cd107d1a2246fcfad5c798aa96fe0fcbb681
juanuranidev <juanuranidev@gmail.com>
2024-06-05 17:18:09 -0300

    Trying to fix manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Entrenaly",
+        short_name: "Entrenaly",
+        icons: [
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        description: "Una aplicación de entrenamiento personal",
+        lang: "es-AR",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
