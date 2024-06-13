39f8ce2c2c76fbd02f8de99a0cceadc3f3420d08
juanuranidev <juanuranidev@gmail.com>
2024-06-13 14:25:24 -0300

    Updated public folder and manifest in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
@@ -10,25 +10,25 @@ export default defineConfig({
         display: "standalone",
         display_override: ["window-controls-overlay"],
         lang: "es-ES",
-        name: "Vite + React PWA",
-        short_name: "Ejemplo PWA",
-        description: "Ejemplo de PWA",
-        theme_color: "#19223c",
-        background_color: "#d4d4d4",
+        name: "Entrenaly",
+        short_name: "Entrenaly",
+        description: "Tus herramientas de trabajo en un solo lugar.",
+        theme_color: "#376EFF",
+        background_color: "#ffffff",
         icons: [
           {
-            src: "pwa-64x64.png",
+            src: "entrenaly-64x64.png",
             sizes: "64x64",
             type: "image/png",
           },
           {
-            src: "pwa-192x192.png",
+            src: "entrenaly-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "pwa-512x512.png",
+            src: "entrenaly-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
@@ -51,5 +51,5 @@ export default defineConfig({
       components: "/src/components",
     },
   },
-  base: "https://entrenaly.vercel.app/",
+  base: "http://localhost:4173/",
 });
