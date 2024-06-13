6cf112c0211137f09389c6b73125c7aff11fae58
juanuranidev <juanuranidev@gmail.com>
2024-06-13 00:42:09 -0300

    Trying to make a pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,42 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        display: "standalone",
+        display_override: ["window-controls-overlay"],
+        lang: "es-ES",
+        name: "Vite + React PWA",
+        short_name: "Ejemplo PWA",
+        description: "Ejemplo de PWA",
+        theme_color: "#19223c",
+        background_color: "#d4d4d4",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
@@ -17,4 +51,5 @@ export default defineConfig({
       components: "/src/components",
     },
   },
+  base: "https://entrenaly.vercel.app/",
 });
