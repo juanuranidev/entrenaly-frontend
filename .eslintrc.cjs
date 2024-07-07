9c450527910bfe2307df8331e7068da50a1f6865
juanuranidev <juanuranidev@gmail.com>
2024-07-07 15:05:01 -0300

    Added eslint rule eqeqeq

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,7 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+    eqeqeq: "error",
     "@typescript-eslint/no-explicit-any": "off",
     "react-refresh/only-export-components": "off",
     "react-hooks/exhaustive-deps": "off",
