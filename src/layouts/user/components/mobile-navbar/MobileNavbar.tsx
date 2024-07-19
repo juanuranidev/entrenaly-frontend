91519792f4ae15ce3408888122882fb5e944219c
juanuranidev <juanuranidev@gmail.com>
2024-07-18 23:58:56 -0300

    Wroking on MobileNavbar

--- src/layouts/user/components/mobile-navbar/MobileNavbar.tsx
+++ src/layouts/user/components/mobile-navbar/MobileNavbar.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import Icons from "lib/utils/icons/icons";
 
 type navbarItem = {
   title: string;
@@ -13,8 +14,20 @@ type Props = {
 export default function MobileNavbar({ navbarItems }: Props) {
   console.log(navbarItems);
   return (
-    <Box>
-      <p>Test</p>
+    <Box
+      style={{
+        position: "fixed",
+        bottom: 0,
+        height: "5rem",
+        backgroundColor: "white",
+        width: "100%",
+      }}
+    >
+      <Stack flexDirection="row" justifyContent="center" alignItems="center">
+        <Icons.person style={{ flexGrow: 1 }} />
+        <Icons.plans style={{ flexGrow: 1 }} />
+        <Icons.dumbbell style={{ flexGrow: 1 }} />
+      </Stack>
     </Box>
   );
 }
