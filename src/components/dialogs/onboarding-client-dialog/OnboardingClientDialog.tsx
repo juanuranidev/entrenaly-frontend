c49e2c8fdefe3541689d8761e0cfba1f1049001c
juanuranidev <juanuranidev@gmail.com>
2024-07-23 23:35:03 -0300

    Working on onboarding client dialog

--- src/components/dialogs/onboarding-client-dialog/OnboardingClientDialog.tsx
+++ src/components/dialogs/onboarding-client-dialog/OnboardingClientDialog.tsx
@@ -1,9 +1,20 @@
+import { Dialog, DialogTitle, Typography } from "@mui/material";
+// import { useState } from "react";
+
 type Props = {
   open: boolean;
   close: () => void;
 };
 
 export default function OnboardingClientDialog({ open, close }: Props) {
+  // const [currentStep, setCurrentStep] = useState<number>(0);
+
   console.log(open, close);
-  return <div>OnboardingClientDialog</div>;
+  return (
+    <Dialog open={open} onClose={close} fullWidth>
+      <DialogTitle display="flex" justifyContent="flex-end">
+        <Typography>Completa tu perfil</Typography>
+      </DialogTitle>
+    </Dialog>
+  );
 }
