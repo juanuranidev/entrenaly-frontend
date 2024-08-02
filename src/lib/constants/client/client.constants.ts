eedc3818e875e283321213fdd7650543a55e44d4
juanuranidev <juanuranidev@gmail.com>
2024-08-01 23:29:05 -0300

    Added clients constants

---
+++ src/lib/constants/client/client.constants.ts
@@ -0,0 +1,26 @@
+export const CLIENT_CONSTANTS = {
+  MEDICAL_INFORMATION: {
+    TYPES_OF_BODIES: {
+      EXTOMORPH: "Ectomorfo",
+      MESOMORPH: "Mesomorfo",
+      ENDOMORPH: "Endomorfo",
+    },
+    TYPES_OF_BODIES_LIST: [
+      {
+        type: "Ectomorfo",
+        description:
+          "Una persona con una estructura corporal generalmente más delgada, con extremidades largas y una complexión que tiende a ser ligera. Este somatotipo puede encontrar más desafiante ganar masa muscular o peso debido a su metabolismo rápido.",
+      },
+      {
+        type: "Mesomorfo",
+        description:
+          "Una persona con una estructura corporal musculosa y atlética, con facilidad tanto para ganar músculo como para perder grasa. Este somatotipo a menudo tiene una complexión más cuadrada o en forma de V",
+      },
+      {
+        type: "Endomorfo",
+        description:
+          "Una persona con una estructura corporal más robusta y redondeada, con una mayor tendencia a almacenar grasa. Este somatotipo puede encontrar más desafiante perder peso, pero a menudo tiene una gran fuerza y capacidad de resistencia",
+      },
+    ],
+  },
+};
